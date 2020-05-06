918ad79a49d665f612d0302fed14e655408ea968
0deepsleep <0deepsleep@gmail.com>
2020-05-06 20:01:54 +0900

    스피너 각 아이템 선택 이벤트 추가 예시

--- app/src/main/java/dasdsa/sdn/editmyinfopractice/MainActivity.kt
+++ app/src/main/java/dasdsa/sdn/editmyinfopractice/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.util.Log.*
+import android.view.View
+import android.widget.AdapterView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -18,7 +20,34 @@ class MainActivity : BaseActivity() {
 
 
     override fun setupEvents() {
-        okBtn.setOnLongClickListener {
+
+        jobSpinner.onItemSelectedListener = object : AdapterView.OnItemClickListener,
+            AdapterView.OnItemSelectedListener {
+            override fun onItemClick(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                Log.d("선택된아이템포지션", "${position}번째 아이템 선택")
+            }
+
+        }
+
+        okBtn.setOnClickListener {
             //스피너에 선택된 문구를 로그 찍기
             val selectedJob = jobSpinner.selectedItem as String
             Log.d ("선택된 직업", selectedJob)
